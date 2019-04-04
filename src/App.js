7b9d045ec3db3a9ea3a98c0c8251e0f717632116
den_b_it@i.ua <1058ark7>
2019-04-04 23:45:02 +0300

    translation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-//import logo from './logo.svg';
 import './App.css';
-import {LanguageList} from './Languages'
+import {LanguageList} from './Languages';
+//import logo from './logo.svg';
 import Header from './Header'
 import Main from './Main'
-import Footer from './Footer'
+import Footer from './Footer';
+export const Language=React.createContext();
 export const menulist=new Array("UA","PL","EN","RU");
 export const title_text="Chemistry CODE";
-const Language=React.createContext(LanguageList.ua);
-console.log(LanguageList);
 
+console.log(LanguageList);
 
 
 class Blank extends Component{
@@ -26,9 +26,18 @@ class Blank extends Component{
 
 
 class App extends Component {
-  render() {
+    constructor(props) {
+        super(props);
+        this.state={language:LanguageList.ua}
+    }
+
+    componentDidMount() {
+        setTimeout(()=>this.setState({language:LanguageList['ua']}),100);
+    }
+
+    render() {
     return (
-        <Language.Provider value={{filler:'філер'}}>
+        <Language.Provider value={ this.state.language} >
       <div className="App">
           <Blank filler="&nbsp;"/>
         <Header/>
