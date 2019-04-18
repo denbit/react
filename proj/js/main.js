ac5dbde44ff8914812c6f0f99401e6d06b29acc2
denbit <den_b_it@i.ua>
2019-04-18 12:16:41 +0300

    source

---
+++ proj/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $('.main__slider').slick({
+    arrows: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    centerMode: true,
+    variableWidth: true
+  });
+});
