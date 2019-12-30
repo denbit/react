d681a2942bfb2afd9f37997142bee470858e8fde
denbitv2 <denys.b@cudev.org>
2019-12-30 18:43:53 +0200

    added cookie

--- src/func.list.js
+++ src/func.list.js
@@ -1,3 +1,13 @@
-export function changeLang() {
-   console.log(this);
+import fallback from "./fallback";
+import config from "./config";
+
+function translate(container, value) {
+	const {defaultLanguage} = config;
+	if (container && container[value]) {
+		return container[value]
+	} else {
+		return fallback.loading[defaultLanguage]
+	}
 }
+
+export {translate};
