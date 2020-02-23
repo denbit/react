f2d42c45e65a3de39dd3bfb8663a48b81990ce6b
r_a <elika.filin>
2020-02-23 02:14:46 +0200

    added to mark up footer with nav

--- src/content/Footer.jsx
+++ src/content/Footer.jsx
@@ -1,1 +1,24 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import style from "../style/Footer.scss"
+import Nav from "./Nav";
+class Footer extends Component {
+    static  propTypes = {
+        contactData: PropTypes.object.isRequired,
+        info: PropTypes.string,
+    };
+    static defaultProps = {info:"Info place with additional data"};
+    render() {
+        return (
+            <footer>
+               <h3 className="title">{this.props.info}</h3>
+                <section className={'data'}>
+                    {this.props.contactData.explain}
+                </section>
+                <Nav navigateTo={this.props.forNav}/>
+            </footer>
+        );
+    }
+}
 
+export default Footer;
