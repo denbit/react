44586c110efccd55ea202dcaa9b6d58ea2e1999e
lodry <jan_ph@op.pl>
2020-03-24 16:12:19 +0200

    Fixed: Copyright.js

--- src/common/Copyright/Copyright.js
+++ src/common/Copyright/Copyright.js
@@ -4,6 +4,7 @@ import React from 'react'
 
 export const Copyright = (props) => {
 	return (<small className={style.copyright}>
-		Developed by <a href="http://www.lgvi.top" target="_blank">Langivi Code technologies &reg;</a>
+		Developed by <a href="http://www.lgvi.top" rel="noopener noreferrer" target="_blank">Langivi Code
+		technologies &reg;</a>
 	</small>);
 };
