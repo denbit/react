39531e913ea465002a1532af54bcc78120af6b50
lodry <jan_ph@op.pl>
2020-06-06 05:40:19 +0300

    second step: add file type input

--- src/content/Main.js
+++ src/content/Main.js
@@ -14,6 +14,7 @@ import Footer from "./Footer";
 import {WrappedProfile} from './Profile/ProfileWrapper';
 import Login from './Login';
 import Logout from './Logout';
+
 let goTo;
 class Main extends Component {
 	constructor(props) {
@@ -91,6 +92,7 @@ class Main extends Component {
 
 
 	renderRouteComponent(element) {
+
 		return (props) => <Screen
 				className={'main_screen'}
 				page={this.state.options[element]}
